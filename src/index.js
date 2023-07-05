75f4996a99cc8c3ad6ea3bbe7b5b2676455fa56b
Rasmus H <99361520+rasmushaislund@users.noreply.github.com>
2023-07-05 12:24:58 +0200

    Add event listeners

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import './style.css';
 import './index.html';
+import { showWeather, toggleTemperature } from './populate';
+
+// Add event listeners for location search
+const location = document.querySelector('#location');
+const search = document.querySelector('.search-btn');
+
+search.addEventListener('click', () => {
+  showWeather(location.value);
+});
+
+location.addEventListener('keypress', (e) => {
+  if (e.keyCode === 13) {
+    search.click();
+  }
+});
+
+// Add event listener for toggle that
+// shows temperature as Celsius or Fahrenheit
+const toggle = document.querySelector('#temp-toggle');
+
+toggle.addEventListener('change', () => {
+  toggleTemperature(toggle.valueAsNumber);
+});
