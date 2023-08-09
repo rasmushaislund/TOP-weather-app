ec3a30b6f04d0735fc63a69f49a8155f9e98eec0
Rasmus H <99361520+rasmushaislund@users.noreply.github.com>
2023-08-09 11:48:28 +0200

    Add console.clear() and trim try/catch content

--- src/fetch.js
+++ src/fetch.js
@@ -5,40 +5,16 @@ import { showError } from './populate';
 export async function fetchWeather(place) {
   const apiKey = '0f5a85c9a97e48cdb5081603232106';
   const urlCurrent = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${place}`;
+  console.clear();
 
   try {
     const response = await fetch(urlCurrent, { mode: 'cors' });
     if (!response.ok) throw new Error();
     const toJSON = await response.json();
-    console.log(toJSON);
     return toJSON;
   } catch (error) {
     showError();
   }
-
-  // Store values from fetch in variables
-  // const lastUpdated = toJSON.current.last_updated;
-  // const icon = toJSON.current.condition.icon;
-  // const tempC = toJSON.current.temp_c;
-  // const tempF = toJSON.current.temp_f;
-  // const description = toJSON.current.condition.text;
-  // const humidity = toJSON.current.humidity;
-  // const windDir = toJSON.current.wind_dir;
-  // const windSpeed = toJSON.current.wind_kph;
-  // const isDay = toJSON.current.is_day;
-  // // Return relevant data to be used elsewhere in the codebase
-  // return {
-  //   toJSON,
-  //   //   lastUpdated,
-  //   //   icon,
-  //   //   tempC,
-  //   //   tempF,
-  //   //   description,
-  //   //   humidity,
-  //   //   windDir,
-  //   //   windSpeed,
-  //   //   isDay,
-  // };
 }
 
 // END //
